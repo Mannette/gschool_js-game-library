a3e66c43c16c1c185e66b6303377caeb75e1ec06
ethan <ethanmannette@gmail.com>
2015-08-11 16:29:53 -0600

    Added basic form for games. Added basic functions in main.js file.

--- js/main.js
+++ js/main.js
@@ -2,4 +2,23 @@
 
 $(document).on('ready', function() {
   console.log('sanity check!');
+
+  var Game = function(title, genre) {
+    this.title = title;
+    this.genre = [];
+  };
+
+  Game.prototype.render = function() {
+
+  };
+
+  var GameLibrary = function(title, games) {
+    this.title = title;
+    this.games = [];
+  };
+
+  GameLibrary.prototype.render = function() {
+
+  };
+
 });
