77a21163c22eb11d96ef79442cb58fb098ac5137
ethan <ethanmannette@gmail.com>
2015-08-12 16:13:02 -0600

    Game can have multiple genres. Added form for adding game library, currently broken.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,64 @@
 // add scripts
 
-$(document).on('ready', function() {
+$(document).ready(function() {
   console.log('sanity check!');
 
   var Game = function(title, genre) {
     this.title = title;
-    this.genre = [];
+    this.genre = genre;
   };
 
-  Game.prototype.render = function() {
+  // Render game when submit button is clicked
+  Game.prototype.render = $('#submit').click(function(event) {
 
-  };
+    event.preventDefault();
 
-  var GameLibrary = function(title, games) {
+    // Create an array to push multiple genres to
+    var genreArr = [];
+    // Select user inputed game title
+    var gameTitle = $('#game-title').val();
+    // Select all checked checkboxes
+      // Creates an array
+    var gameGenre = $('.genre:checked');
+
+    // Iterates through gameGenre array.
+    $.each(gameGenre, function(i) {
+      // debugger;
+      // Pulls value from index and pushes it to array.
+      genreArr[i] = $(this).val();
+      console.log(genreArr);
+    });
+
+    // Sets each gameTitle to a new game.
+    gameTitle = new Game(gameTitle, genreArr);
+    console.log(gameTitle);
+
+    // Clears inputs.
+    gameGenre.removeAttr('checked');
+    $('#game-title').val('');
+
+  });
+
+  var GameLibrary = function(title) {
     this.title = title;
     this.games = [];
   };
 
-  GameLibrary.prototype.render = function() {
+  GameLibrary.prototype.render = $('#libraryButton').click(function() {
 
-  };
+    // Create new library
+    // Give library name
+    // Add games to selected library
+
+    var library = $('#newLibrary');
+
+  });
+
+
+
+
+
+
+
 
 });
---
+++ js/utility.js
@@ -0,0 +1,29 @@
+// var Game = function(title, genre) {
+//   this.title = title;
+//   this.genre = genre;
+// };
+//
+// Game.prototype.render = function() {
+//   // Create an array to push multiple genres to
+//   var genreArr = [];
+//   // Select user inputed game title
+//   var gameTitle = $('#game-title').val();
+//   // Select all checked checkboxes
+//     // Creates an array
+//   var gameGenre = $('.genre:checked');
+//   // Push each genre to an array
+//   $.each(gameGenre, function(i) {
+//     // debugger;
+//     genreArr[i] = $(this).val();
+//     console.log(genreArr);
+//   });
+// };
+//
+// var GameLibrary = function(title) {
+//   this.title = title;
+//   this.games = [];
+// };
+//
+// GameLibrary.prototype.render = function() {
+//
+// };
